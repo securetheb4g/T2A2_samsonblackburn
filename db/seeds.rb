010e4734472d3594d2f773cdcdcdb4fff333958f
Samson Blackburn <Samson@Samsons-MBP.lan>
2020-11-13 20:44:30 +1000

    added products through rail c, also added same three products through seed to make sure both working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create(name:"DonabeSmoker", price: 250.00)
+Product.create(name:"DonabeSteamer 1.2L", price: 250.00)
+Product.create(name:"DonabeSteamer 2.5L", price: 340.00)
