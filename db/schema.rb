a67fae307f177be4e0a43b3571db8dc17b4f47a1
Samson Blackburn <Samson@Samsons-MBP.lan>
2020-11-13 20:22:46 +1000

    generated scaffold model Product, crud functionality, database migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_122144) do
+ActiveRecord::Schema.define(version: 2020_11_13_101951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string "name"
