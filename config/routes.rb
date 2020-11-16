a4cf2579b1fa2e9e1a15bf8d3458faaa22e76cb3
Samson Blackburn <Samson@Samsons-MBP.lan>
2020-11-17 04:05:39 +1000

    commit before Heroku push, final basic deployment test before attempting complicated features

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post "/show/:id/buy", to: "products#buy", as: "buy"
-  # post "/show/:id/buy", to: "products#buy", as: "buy"
   get "/show/:id/success", to: "products#success", as: "success"
   get "/show/:id/cancel", to: "products#cancel", as: "cancel"
 
