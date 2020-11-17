03ec55be55ad52017e015e2b71479e7517874db8
Samson Blackburn <Samson@Samsons-MacBook-Pro.local>
2020-11-17 15:49:38 +1000

    push before heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,25 +15,30 @@ User.first.add_role(:admin)
 user = User.create!(email: "user@test", password: "password")
 
 Product.destroy_all
-product1 = Product.create!(name: "Donabe Smoker" , price: 200, user_id: user.id)
-product2 = Product.create!(name: "Donabe Multipurpose" , price: 250, user_id: user.id)
-product3 = Product.create!(name: "Donabe Mustard" , price: 130, user_id: user.id)
-
-puts "users created: #{User.count}"
-puts "products created: #{Product.count}"
-
+product1 = Product.new(name: "Donabe Smoker" , price: 200, user_id: user.id)
 product1.image.attach(
     io: File.open(Rails.root.join("app", "assets", "images", "Donabesmoker.jpg")), 
     filename: 'Donabesmoker.jpg',
     content_type: "image/jpg"
 )
+product1.save
+
+product2 = Product.new!(name: "Donabe Multipurpose" , price: 250, user_id: user.id)
 product2.image.attach(
     io: File.open(Rails.root.join("app", "assets", "images", "DonabeMultipurpose.jpg")), 
     filename: 'Donabesmoker.jpg',
     content_type: "image/jpg"
 )
+product2.save
+
+product3 = Product.create!(name: "Donabe Mustard" , price: 130, user_id: user.id)
 product3.image.attach(
     io: File.open(Rails.root.join("app", "assets", "images", "Donabemustard.jpg")), 
     filename: 'Donabesmoker.jpg',
     content_type: "image/jpg"
 )
+product3.save
+
+puts "users created: #{User.count}"
+puts "products created: #{Product.count}"
+
